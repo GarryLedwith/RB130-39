ba633cfd09b2d7d180a0babce6345099d14537e8
Garry Ledwith <garryledwith@gmail.com>
2022-05-05 17:37:12 +0100

    Easy challenges

--- Ruby_challenges/easy_challenges/anagram.rb
+++ Ruby_challenges/easy_challenges/anagram.rb
@@ -1,94 +1,86 @@
 =begin
 
-PROBLEM: 
+PROBLEM:
+Input: string
+Output: array of anagrams
 
-Write a program that takes a word and a list of possible anagrams and selects the correct sublist that contains the anagrams of the word.
+Rules/Requirements
+- An anagram is a word or phrase formed by
+rearragning the letters of a different word or phrases
+- write a program that takes a word
+(string) and a list of possible anagrams (array)
+and select the corrent sublist that contains the anagram of the word
+- if no anagrams are found then return an empty array
+  - Else return an array of sorted anagrams
+- remove any duplicates in annagram array
 
-For example, given the word "listen" and a list of candidates like "enlists", "google", "inlets", and "banana", the program should return a list containing "inlets". Please read the test suite for the exact rules of anagrams.
-
-
-Input: string 
-Output: array of anagrams 
-
-
-Rules/Requirements 
-- An anagram is a word or phrase formed by rearragning the letters of a different word or phrase 
-- write a program that takes a word (string) and a list of possible anagrams (array) and select the corrent sublist that contains the anagram of the word 
-- if no anagrams are found then return an empty array 
-  - Else return an array of sorted anagrams 
-- remove any duplicates in annagram array 
-- 
-
-
-
-Questions: 
-How to I deal with case? 
-  Does 'Corn' == 'corn'? 
-  Case does not matter
-
-
+Questions:
+How to I deal with case?
+Does 'Corn' == 'corn'?
+Case does not matter
 ------------------
-EXAMPLES: 
+EXAMPLES:
 
 "listen" == 'silent'
 
-
-
-
 ------------------
-DATA STRUCTURES: 
-String 
-Array 
-
+DATA STRUCTURES:
+String
+Array
 
 ---
 
 ALGORITHM:
 
-=> Goal: Return an array with a list of anagrams 
+=> Goal: Return an array with a list of anagrams
 
-=> Steps: 
+=> Steps:
 
 == anagrams method ====
-- method takes two arguments: a string and an array 
-- create a hash called results with the key as a sorted word an array as the value 
+- method takes two arguments: a string and an array
+- create a hash called results with the key as a sorted word
+and array as the value
 - loop through input array and pass in each word to the block
-if key equals the current word 
+if key equals the current word
   then add word to value of results hash
-elsif 
+end
+return array of anagrams
 
+=== match method ====
+takes an array as an argument
+call anagrams method to test if arrays match
 
-
-=== match method ==== 
-takes an array as an argument 
- 
-
+=== sorted_letters ===
+takes string as arguement
+lowercase string
+convert into an array of chars
+sort, and convert back to string
 =end
 
 class Anagram
   def initialize(word)
     @word = word.downcase
-  end 
+  end
 
   def match(arr)
     anagrams(@word, arr)
   end
 
-  private 
+  private
 
   def sorted_letters(str)
-    str.downcase.chars.sort.join 
+    str.downcase.chars.sort.join
   end
 
   def anagrams(str, arr)
     results = Hash.new([])
     key = sorted_letters(str)
-  
+
     arr.each do |word|
       if sorted_letters(word) == key
-        results[key] << word 
+        results[key] << word
       end
-    end 
+    end
     results[key].reject { |word| word.downcase == str }
   end
 end
